772a2018ad8f012b5601cc429afb2310472af051
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-11-17 17:27:26 +0100

    update diauske scripts

--- config_vollseg.py
+++ config_vollseg.py
@@ -9,16 +9,11 @@ class Params:
         batch_size: int 
         patch_x: int 
         patch_y: int 
-        patch_z: int 
         kern_size: int 
         n_patches_per_image: int 
-        n_rays: int 
         startfilter: int 
         erosion_iterations: int
-        use_gpu_opencl: bool 
         generate_npz: bool 
-        backbone: str 
-        load_data_sequence: bool 
         validation_split: float 
         n_channel_in: int 
         train_unet: bool 
@@ -30,37 +25,23 @@ class Params:
         max_size: int 
         n_tiles: tuple
         axes: str
-        dounet: bool 
-        seedpool: bool  
-        slice_merge: bool
-        UseProbability: bool 
-        donormalize: bool 
-        ExpandLabels: bool
-        
+               
     
 @dataclass
 class Paths: 
     model_dir: str 
-    star_model_dir:  str
     unet_model_dir:  str
-    roi_model_dir:  str
-    den_model_dir:  str
     predict_image_dir: str
     save_dir: str 
     base_dir: str
     raw_dir: str
-    real_mask_dir: str
     binary_mask_dir: str
     
 @dataclass
 class Files:  
     npz_filename: str
-    model_name: str 
-    star_model_name: str
     unet_model_name: str
-    roi_model_name: str
-    den_model_name: str 
-    
+       
     
     
 @dataclass
--- predict_xenopus_vollseg.py
+++ predict_xenopus_vollseg.py
@@ -18,15 +18,8 @@ def main( config : VollSegConfig):
                 save_dir = config.paths_vollseg.save_dir
                 
                 unet_model_name = config.files_vollseg.unet_model_name
-                star_model_name = config.files_vollseg.star_model_name
-                roi_model_name = config.files_vollseg.roi_model_name
-
-
                 unet_model = UNET(config = None, name = unet_model_name, basedir = model_dir)
-                star_model = StarDist3D(config = None, name = star_model_name, basedir = model_dir)
-                roi_model =  MASKUNET(config = None, name = roi_model_name, basedir = model_dir)
-
-
+                
 
                 Raw_path = os.path.join(image_dir, config.params.file_type)
                 filesRaw = glob.glob(Raw_path)
@@ -36,31 +29,19 @@ def main( config : VollSegConfig):
                 min_size_mask = config.params.min_size_mask
                 max_size = config.params.max_size
                 n_tiles = config.params.n_tiles
-                dounet = config.params.dounet
-                seedpool = config.params.seedpool
-                slice_merge = config.params.slice_merge
-                UseProbability = config.params.UseProbability
-                donormalize = config.params.donormalize
                 axes = config.params.axes
-                ExpandLabels = config.params.ExpandLabels
+                
                 for fname in filesRaw:
                 
                                 image = imread(fname)
                                 Name = os.path.basename(os.path.splitext(fname)[0])
                                 VollSeg( image, 
                                         unet_model = unet_model, 
-                                        star_model = star_model, 
-                                        roi_model= roi_model,
-                                        seedpool = seedpool, 
                                         axes = axes, 
                                         min_size = min_size,  
                                         min_size_mask = min_size_mask,
                                         max_size = max_size,
-                                        donormalize=donormalize,
                                         n_tiles = n_tiles,
-                                        ExpandLabels = ExpandLabels,
-                                        slice_merge = slice_merge, 
-                                        UseProbability = UseProbability, 
                                         save_dir = save_dir, 
                                         Name = Name,
                                         dounet = dounet)   
